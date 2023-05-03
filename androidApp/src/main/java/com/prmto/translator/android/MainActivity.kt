3d9ca4611d21529394acac4dfb99ee6b5072b9c7
Tolga Pirim <tolgapirim25@gmail.com>
2023-05-03 18:52:03 +0300

    Implemented error handling and we can navigate to VoiceToText Screen

--- androidApp/src/main/java/com/prmto/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/prmto/translator/android/MainActivity.kt
@@ -6,17 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.prmto.translator.android.core.presentation.Routes
 import com.prmto.translator.android.translate.presentation.AndroidTranslateViewModel
 import com.prmto.translator.android.translate.presentation.TranslateScreen
+import com.prmto.translator.translate.presentation.TranslateEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -49,8 +53,33 @@ fun TranslateRoot() {
             val state by viewModel.state.collectAsState()
             TranslateScreen(
                 state = state,
-                onEvent = viewModel::onEvent
+                onEvent = { event ->
+                    when (event) {
+                        is TranslateEvent.RecordAudio -> {
+                            navController.navigate(
+                                route = Routes.VOICE_TO_TEXT + "/${state.fromLanguage.language.langCode}"
+                            )
+                        }
+
+                        else -> viewModel.onEvent(event)
+                    }
+                }
             )
+        }
+
+        composable(
+            route = Routes.VOICE_TO_TEXT + "/{languageCode}",
+            arguments = listOf(
+                navArgument(
+                    name = "langugeCode"
+                ) {
+                    type = NavType.StringType
+                    defaultValue = "en"
+                }
+            )
+        ) {
+            Text(text = "Voice to text screen")
         }
     }
 }
+
--- androidApp/src/main/java/com/prmto/translator/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/prmto/translator/android/translate/presentation/TranslateScreen.kt
@@ -8,19 +8,26 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.prmto.translator.android.R
@@ -29,6 +36,7 @@ import com.prmto.translator.android.translate.presentation.components.SwapLangua
 import com.prmto.translator.android.translate.presentation.components.TranslateHistoryItem
 import com.prmto.translator.android.translate.presentation.components.TranslateTextField
 import com.prmto.translator.android.translate.presentation.components.rememberTextToSpeech
+import com.prmto.translator.translate.domain.translate.TranslateError
 import com.prmto.translator.translate.presentation.TranslateEvent
 import com.prmto.translator.translate.presentation.TranslateState
 import java.util.Locale
@@ -40,10 +48,44 @@ fun TranslateScreen(
     onEvent: (TranslateEvent) -> Unit
 ) {
     val context = LocalContext.current
+
+
+    LaunchedEffect(key1 = state.error) {
+        val message = when (state.error) {
+            TranslateError.SERVICE_UNAVAILABLE -> context.getString(R.string.error_service_unavaliable)
+            TranslateError.ClIENT_ERROR -> context.getString(R.string.client_error)
+            TranslateError.SERVER_ERROR -> context.getString(R.string.server_error)
+            TranslateError.UNKNOWN_ERROR -> context.getString(R.string.unknown_error)
+            else -> null
+        }
+
+        message?.let {
+            Toast.makeText(
+                context,
+                message,
+                Toast.LENGTH_LONG
+            ).show()
+            onEvent(TranslateEvent.OnErrorSeen)
+        }
+    }
+
     Scaffold(
         floatingActionButton = {
-
-        }
+            FloatingActionButton(
+                onClick = {
+                    onEvent(TranslateEvent.RecordAudio)
+                },
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.size(75.dp)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.mic),
+                    contentDescription = stringResource(id = R.string.record_audio)
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) { padding ->
         LazyColumn(
             modifier = Modifier
