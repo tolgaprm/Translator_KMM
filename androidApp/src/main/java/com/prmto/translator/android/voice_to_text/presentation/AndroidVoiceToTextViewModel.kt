e8509b2c25d4f68ffb81d7a3d22e85b0da94f4f7
Tolga Pirim <tolgapirim25@gmail.com>
2023-05-08 14:16:08 +0300

    Implemented VoiceToTextViewModel

---
+++ androidApp/src/main/java/com/prmto/translator/android/voice_to_text/presentation/AndroidVoiceToTextViewModel.kt
@@ -0,0 +1,29 @@
+package com.prmto.translator.android.voice_to_text.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prmto.translator.voice_to_text.domain.VoiceToTextParser
+import com.prmto.translator.voice_to_text.presentation.VoiceToTextEvent
+import com.prmto.translator.voice_to_text.presentation.VoiceToTextViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AndroidVoiceToTextViewModel @Inject constructor(
+    private val parser: VoiceToTextParser
+) : ViewModel() {
+
+    private val viewModel by lazy {
+        VoiceToTextViewModel(
+            parser = parser,
+            coroutineScope = viewModelScope
+        )
+    }
+
+    val state = viewModel.state
+
+    fun onEvent(event: VoiceToTextEvent) {
+        viewModel.onEvent(event)
+    }
+
+}
