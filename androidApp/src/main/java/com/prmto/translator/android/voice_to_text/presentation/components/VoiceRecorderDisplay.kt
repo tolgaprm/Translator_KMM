b34acfa3d7c2f8e768b8823d9979f2b64067163f
Tolga Pirim <tolgapirim25@gmail.com>
2023-05-08 12:57:29 +0300

    Implemented Android voice recorder display

---
+++ androidApp/src/main/java/com/prmto/translator/android/voice_to_text/presentation/components/VoiceRecorderDisplay.kt
@@ -0,0 +1,94 @@
+package com.prmto.translator.android.voice_to_text.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawscope.clipRect
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.prmto.translator.android.TranslatorTheme
+import com.prmto.translator.android.translate.presentation.components.gradientSurface
+import java.util.Random
+
+@Composable
+fun VoiceRecorderDisplay(
+    powerRatios: List<Float>,
+    modifier: Modifier = Modifier,
+) {
+    val primary = MaterialTheme.colors.primary
+    Box(
+        modifier = modifier
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .clip(RoundedCornerShape(20.dp))
+            .gradientSurface()
+            .padding(
+                horizontal = 32.dp,
+                vertical = 8.dp
+            )
+            .drawBehind {
+                val powerRatioWidth = 3.dp.toPx() // Canvas deals with pixel value
+                val powerRatioCount = (size.width / (powerRatioWidth * 2)).toInt()
+                // The count of power ratios we can draw on the canvas
+                // The reason we divide by 2 is because we want to leave some space between each bar
+
+                clipRect(
+                    left = 0f,
+                    top = 0f,
+                    right = size.width,
+                    bottom = size.height
+                ) {
+                    // We clip the canvas so that we don't draw outside of the canvas
+                    powerRatios
+                        .takeLast(powerRatioCount)
+                        .reversed()
+                        .forEachIndexed { i, ratio ->
+                            val yTopStart = center.y - (size.height / 2) * ratio
+                            drawRoundRect(
+                                color = primary,
+                                topLeft = Offset(
+                                    x = size.width - i * powerRatioWidth * 2,
+                                    y = yTopStart
+                                ),
+                                size = Size(
+                                    width = powerRatioWidth,
+                                    height = (center.y - yTopStart) * 2
+                                ),
+                                cornerRadius = CornerRadius(100f)
+                            )
+                        }
+                }
+            }
+    ) {
+
+    }
+}
+
+
+@Preview
+@Composable
+fun VoiceRecorderDisplayPreview() {
+    TranslatorTheme {
+        VoiceRecorderDisplay(
+            powerRatios = (0..100).map {
+                Random().nextFloat()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+    }
+}
