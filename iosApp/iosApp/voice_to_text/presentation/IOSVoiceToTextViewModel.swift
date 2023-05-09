20dd5df76971e917fd8d2fe9471e9db9c9d86766
Tolga Pirim <tolgapirim25@gmail.com>
2023-05-09 14:08:27 +0300

    Implemented IOSVoiceToTextViewModel

---
+++ iosApp/iosApp/voice_to_text/presentation/IOSVoiceToTextViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  VoiceToTextViewModel.swift
+//  iosApp
+//
+//  Created by Tolga Pirim on 9.05.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+import Combine
+
+
+extension VoiceToTextScreen{
+   @MainActor class IOSVoiceToTextViewModel : ObservableObject{
+        private let parser: VoiceToTextParser
+        private let languageCode: String
+       
+        private let viewModel: VoiceToTextViewModel
+       
+       @Published var state = VoiceToTextState(
+        powerRatios: [],
+        spokenText:"",
+        canRecord: false,
+        recordError: nil,
+        displayState: nil
+       )
+       
+       private var handle: DisposableHandle?
+        
+       init(parser: VoiceToTextParser, languageCode: String) {
+            self.parser = parser
+            self.languageCode = languageCode
+            self.viewModel = VoiceToTextViewModel(parser: parser, coroutineScope: nil)
+        }
+        
+        func onEvent(event:VoiceToTextEvent){
+            self.viewModel.onEvent(event: event)
+        }
+        
+       func startObserving(){
+           handle = viewModel.state.subscribe{ [weak self] state in
+               if let  state {
+                   self?.state = state
+               }
+           }
+       }
+       
+       func dispose(){
+           handle?.dispose()
+           viewModel.onEvent(event: VoiceToTextEvent.Reset())
+       }
+    }
+}
---
+++ iosApp/iosApp/voice_to_text/presentation/VoiceToTextScreen.swift
@@ -0,0 +1,21 @@
+//
+//  VoiceToTextScreen.swift
+//  iosApp
+//
+//  Created by Tolga Pirim on 9.05.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct VoiceToTextScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VoiceToTextScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        VoiceToTextScreen()
+    }
+}
